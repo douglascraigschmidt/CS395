c165ca9a0addc7f9a84d8586b9c02c512f86d2fd
douglascraigschmidt <douglas.craig.schmidt@gmail.com>
2021-07-13 15:30:28 -0500

    updates

---
+++ assignment-3/image-crawler/src/main/java/edu/vanderbilt/imagecrawler/crawlers/ReactorCrawler.java
@@ -0,0 +1,252 @@
+package edu.vanderbilt.imagecrawler.crawlers;
+
+import java.net.URL;
+import java.util.Optional;
+
+import edu.vanderbilt.imagecrawler.utils.Crawler;
+import edu.vanderbilt.imagecrawler.utils.Image;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import static edu.vanderbilt.imagecrawler.utils.Crawler.Type.IMAGE;
+import static edu.vanderbilt.imagecrawler.utils.Crawler.Type.PAGE;
+
+/**
+ * This class uses Project Reactor Flux and Mono reactive types to
+ * perform a concurrent image crawl starting from a root Uri.  Images
+ * from HTML pages reachable from the root Uri are downloaded from a
+ * remote web server or from the local file system, transformed, and
+ * then stored in files that can be displayed to the user.
+ */
+public class ReactorCrawler
+       extends ImageCrawler {
+    /**
+     * Recursively crawls the given page and returns the total number
+     * of processed images.
+     *
+     * @param pageUri The URI that's being crawled at this point
+     * @param depth   The current depth of the recursive processing
+     * @return The number of images processed at this depth
+     */
+    @Override
+    protected int performCrawl(String pageUri, int depth) {
+        // Call the crawlPageAsync() helper method to perform the
+        // crawl asynchronously, obtain a count of the number of
+        // images downloaded/processed, wait until all the processing
+        // is done, and return the result as an int value.  If any
+        // error/exception is encountered return 0.
+
+        // TODO -- you fill in here replacing this statement with your
+        // solution.
+        return 0;
+            // Crawl the page using the specified uri and depth.
+            
+
+            // Count the number of elements in the stream of
+            // observables.
+            
+
+            // Return 0 if any exception is encountered.
+            
+
+            // Block to get the result.
+            
+
+            // Convert the result to an int primitive.
+            
+    }
+
+    /**
+     * Recursively crawls the web page identified by the passed {@code
+     * pageUri} and downloads and transforms all discovered images.
+     * The recursion ends when the passed depth is exceeded.
+     *
+     * @param pageUri The uri to crawl
+     * @param depth   The maximum depth of the crawl
+     * @return A {@link Flux} that emits transformed images
+     */
+    protected Flux<Image> crawlPageAsync(String pageUri, int depth) {
+        // Return a Flux stream of images from this page and all page
+        // links recursively reachable from it.  Return an empty
+        // Observable if the depth limit of the web crawling is
+        // reached or if the pageUri has already been visited.  This
+        // method should use Flux operators like just(), filter(),
+        // map(), and flatMap() (this latter operator should call
+        // imagesOnPageAndPageLinksAsync()).
+
+        // TODO -- you fill in here replacing this statement with your
+        // solution.
+        return null;
+            // Create an Observable that emits this pageUri.
+            
+
+            // Filter out page if it exceeds the depth or has already
+            // been visited.
+            
+
+            // Map the url to a page.
+            
+
+            // Apply the flatMap() concurrency idiom to convert each
+            // page to an Observable stream of images asynchronously.
+            
+    }
+
+    /**
+     * Returns all images on the page and its linked pages.
+     *
+     * @param page  The crawler page to crawl
+     * @param depth The maximum depth of the crawl
+     * @return A {@link Flux} that emits all images on the page and its linked pages
+     */
+    protected Flux<Image> imagesOnPageAndPageLinksAsync(Crawler.Page page,
+                                                        int depth) {
+        // Return a Flux stream consisting of all images on this page
+        // and any images linked on it.  This method should call
+        // imagesOnPageLinks() and imagesOnPage() and use Flux
+        // operators mergeWith() and subscribeOn() to merge all the
+        // images into a single Flux stream via a parallel Scheduler.
+
+        // TODO -- you fill in here replacing this statement with your
+        // solution.
+        return null;
+    }
+
+    /**
+     * Find all page links on the passed page and recursively crawl
+     * each page to download and transform all discovered images.
+     *
+     * @param page The page to search for page links
+     * @return A {@link Flux} that emits non-null image transforms
+     */
+    protected Flux<Image> imagesOnPageLinksAsync(Crawler.Page page,
+                                                 int depth) {
+        // Create and return a Flux stream consisting of images that
+        // have been downloaded and transformed.  This method should
+        // call crawlPageAsync() recursively and use Flux operators
+        // like fromIterable(), flatMap(), and subscribeOn() to
+        // perform the processing on the parallel Scheduler.
+
+        // TODO -- you fill in here replacing this statement with your
+        // solution.
+        return null;
+            // Convert the list of page links into an Observable
+            // stream of page links.
+            
+
+            // Apply the flatMap() concurrency idiom to map each page
+            // to a stream of images that are downloaded and
+            // transformed concurrently via the parallel scheduler.
+        
+    }
+
+    /**
+     * Find, download, and transform all images on the given page.
+     *
+     * @param page The page to search for image urls
+     * @return A {@link Flux} that emits image transforms
+     */
+    protected Flux<Image> imagesOnPageAsync(Crawler.Page page) {
+        // Create and return a Flux stream for this page using the
+        // Flux flatMap() concurrency idiom.  It should call helper
+        // methods getPageElementsAsUrls(), downloadImageAsync(), and
+        // transformImageAsync(), as well as use Flux operators like
+        // flatmap() and fromIterable() perform processing on the
+        // parallel scheduler.  The flatMap() operator can be called
+        // more than once.
+
+        // TODO -- you fill in here replacing this statement with your
+        // solution.
+        return null;
+            // Convert the list of images in the page into an
+            // Flux stream.
+            
+
+            // Apply the flatMap() concurrency idiom to download
+            // the stream of images in parallel.
+            
+
+            // Again apply the flatMap() concurrency idiom to convert
+            // the stream of downloaded images into a stream of images
+            // are transformed in parallel.
+            
+    }
+
+    /**
+     * Asynchronously download and return the image at the given
+     * {@code url}
+     *
+     * @param url The URL to download
+     * @return A {@link Mono} that emits a single downloaded image
+     */
+    protected Mono<Image> downloadImageAsync(URL url) {
+        // Create and return a Mono instance that emits the download
+        // image at the specified URL. It should call the helper
+        // method getOrDownloadImage(), as well as use Mono operators
+        // like fromCallable(), map(), filter(), and subscribeOn() to
+        // perform processing on the parallel scheduler.  The map()
+        // operator can be called more than once.  In addition, Java
+        // Optional methods (such as ofNullable(), isPresent(), and
+        // get()) should be used to avoid dealing with null values.
+
+        // TODO -- you fill in here replacing this statement with your
+        // solution.
+        return null;
+            // Create an Observable that emits the URL.
+            
+
+            // Run computation in the parallel scheduler.
+            
+
+            // Map the image URL to a possibly downloaded image.
+            
+
+            // Only continue processing if an image was available.
+            
+
+            // Convert optionals into values.
+            
+    }
+
+    /**
+     * Applies the current set of crawler transforms on the passed
+     * image and returns an Observable stream of all successfully
+     * transformed images.
+     *
+     * @param image The image to transform
+     * @return A {@link Flux} that emits non-null transformed images
+     */
+    protected Flux<Image> transformImageAsync(Image image) {
+        // Return a Flux stream of transformed images.  This method
+        // should call the createNewCacheItem() and applyTransform()
+        // helper methods, as well as use Flux operators like
+        // fromIterable(), filter(), and map() (filter() and map() can
+        // be called more than once).  In addition, Java Optional
+        // methods (such as ofNullable(), isPresent(), and get())
+        // should be used to avoid dealing with null values.
+
+        // TODO -- you fill in here replacing this statement with your
+        // solution.
+        return null;
+            // Convert the List of transforms into an stream of
+            // transforms.
+            
+
+            // Run computations in the parallel scheduler.
+            
+
+            // Only transform images that haven't already been
+            // transformed.
+            
+
+            // Apply the transform on the image.
+            
+
+            // Filter out any null returns from applyTransform().
+            
+
+            // Convert optionals into values.
+            
+    }
+}
---
+++ assignment-3/image-crawler/src/test/java/edu/vanderbilt/imagecrawler/crawlers/ReactorCrawlerTests.kt
@@ -0,0 +1,339 @@
+package edu.vanderbilt.imagecrawler.crawlers
+
+import admin.AssignmentTests
+import admin.injectInto
+import edu.vanderbilt.imagecrawler.transforms.Transform
+import edu.vanderbilt.imagecrawler.utils.*
+import io.mockk.*
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.SpyK
+import org.assertj.core.api.Assertions.assertThat
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
+import org.junit.Test
+import java.net.URL
+import java.util.*
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+class ReactorCrawlerTests : AssignmentTests() {
+    @SpyK
+    var spyCrawler: ReactorCrawler =
+        ReactorCrawler()
+
+    @MockK
+    lateinit var mockImageFlux: Flux<Image>
+
+    @MockK
+    lateinit var mockOptionalImageFlux: Flux<Optional<Image>>
+
+    @MockK
+    lateinit var mockOptionalImageMono: Mono<Optional<Image>>
+
+    @MockK
+    lateinit var mockImageMono: Mono<Image>
+
+    @MockK
+    lateinit var mockTransformFlux: Flux<Transform>
+
+    @MockK
+    lateinit var mockImage: Image
+
+    @MockK
+    lateinit var mockLongMono: Mono<Long>
+
+    @MockK
+    lateinit var mockTransforms: List<Transform>
+
+    @MockK
+    lateinit var mockStringArray: CustomArray<String>
+
+    @MockK
+    lateinit var mockCrawlerPageFlux: Flux<Crawler.Page>
+
+    @MockK
+    lateinit var mockUrlFlux: Flux<URL>
+
+    @MockK
+    lateinit var mockUrlMono: Mono<URL>
+
+    @MockK
+    lateinit var mockPage: Crawler.Page
+
+    @MockK
+    lateinit var mockStringFlux: Flux<String>
+
+    @Test
+    fun performCrawl() {
+        every { mockImageFlux.count() } returns mockLongMono
+        every { mockLongMono.block() } returns 1
+        every { mockLongMono.onErrorReturn(any()) } returns mockLongMono
+        every { spyCrawler.crawlPageAsync(any(), any()) } returns mockImageFlux
+
+        // SUT
+        spyCrawler.performCrawl("test", 3)
+
+        verify { mockImageFlux.count() }
+        verify { spyCrawler.crawlPageAsync(any(), 3) }
+        verify { mockLongMono.block() }
+        verify { mockLongMono.onErrorReturn(0L) }
+    }
+
+    @Test
+    fun `crawlPageAsync uses the expected Project Reactor outer chained method calls`() {
+        mockkStatic(Flux::class)
+        val mockWebPageCrawler = mockk<WebPageCrawler>()
+        mockWebPageCrawler.injectInto(spyCrawler)
+        every { mockCrawlerPageFlux.flatMap<Image>(any()) } returns mockImageFlux
+        every { mockStringFlux.map<Crawler.Page>(any()) } returns mockCrawlerPageFlux
+        every { mockStringFlux.filter(any()) } returns mockStringFlux
+        every { Flux.just<String>(any()) } returns mockStringFlux
+
+        // SUT
+        spyCrawler.crawlPageAsync("asdf", 3).toStream().forEach() {}
+
+        verify { mockCrawlerPageFlux.flatMap<Image>(any()) }
+        verify { mockStringFlux.filter(any()) }
+        verify { Flux.just<String>(any()) }
+        verify { mockStringFlux.map<Crawler.Page>(any()) }
+    }
+
+    @Test
+    fun `crawlPageAsync uses the expected Project Reactor inner lambda calls`() {
+        val mockWebPageCrawler = mockk<WebPageCrawler>()
+        spyCrawler.mMaxDepth = Int.MAX_VALUE
+        mockWebPageCrawler.injectInto(spyCrawler)
+        val mockHashSet = mockk<ConcurrentHashSet<String>>()
+        mockHashSet.injectInto(spyCrawler)
+        every { mockHashSet.putIfAbsent(any()) } returns true
+        every { mockWebPageCrawler.getPage(any()) } returns mockPage
+
+        // SUT
+        spyCrawler.crawlPageAsync("asdf", 3).toStream().forEach() {}
+
+        verify { mockHashSet.putIfAbsent(any()) }
+        verify { mockWebPageCrawler.getPage(any()) }
+        verify { spyCrawler.imagesOnPageLinksAsync(mockPage, 3) }
+    }
+
+    @Test
+    fun `imagesOnPageAndPageLinksAsync uses the expected Project Reactor outer chained method calls`() {
+        every { mockImageFlux.mergeWith(mockImageFlux) } returns mockImageFlux
+        every { spyCrawler.imagesOnPageLinksAsync(any(), any()) } returns mockImageFlux
+        every { spyCrawler.imagesOnPageAsync(any()) } returns mockImageFlux
+        every { mockImageFlux.subscribeOn(any()) } returns mockImageFlux
+
+        // SUT
+        assertNotNull(spyCrawler.imagesOnPageAndPageLinksAsync(mockPage, 0))
+
+        verify { mockImageFlux.mergeWith(mockImageFlux) }
+        verify { spyCrawler.imagesOnPageAsync(mockPage) }
+        verify { mockImageFlux.subscribeOn(Schedulers.parallel()) }
+        verify { spyCrawler.imagesOnPageLinksAsync(mockPage, 0) }
+    }
+
+    @Test
+    fun `imagesOnPageAndPageLinksAsync uses the expected Project Reactor inner chained method calls`() {
+        every { spyCrawler.imagesOnPageAsync(any()) } returns Flux.just(mockImage)
+        every { spyCrawler.imagesOnPageLinksAsync(any(), any()) } returns Flux.just(mockImage)
+
+        // SUT
+        assertNotNull(spyCrawler.imagesOnPageAndPageLinksAsync(mockPage, 3))
+
+        verify { spyCrawler.imagesOnPageAsync(mockPage) }
+        verify { spyCrawler.imagesOnPageLinksAsync(mockPage, 3) }
+    }
+
+    @Test
+    fun `imagesOnPageLinksAsync uses the expected Project Reactor outer chained method calls`() {
+        mockkStatic(Flux::class)
+        val spyImageFlux = spyk<Flux<Image>>()
+        every { mockStringFlux.flatMap<Image>(any()) } returns spyImageFlux
+        every { Flux.fromIterable<String>(any()) } returns mockStringFlux
+        every { mockPage.getPageElementsAsStrings(Crawler.Type.PAGE) } returns mockStringArray
+
+        // SUT
+        assertNotNull(spyCrawler.imagesOnPageLinksAsync(mockPage, 3))
+
+        verify(exactly = 1) { Flux.fromIterable<String>(any()) }
+        verify(exactly = 1) { mockStringFlux.flatMap<Image>(any()) }
+        verify(exactly = 1) { mockPage.getPageElementsAsStrings(Crawler.Type.PAGE) }
+        verify(exactly = 0) { spyImageFlux.subscribeOn(any()) }
+    }
+
+    @Test
+    fun `imagesOnPageLinksAsync uses the expected Project Reactor inner chained method calls`() {
+        every {
+            mockPage.getPageElementsAsStrings(any())
+        } returns UnsynchronizedArray(listOf("a", "b", "c"))
+
+        val o = mockk<Flux<Image>>()
+
+        every {
+            o.subscribeOn(any())
+        } returns Flux.just(mockImage, mockImage, mockImage)
+
+        every { spyCrawler.crawlPageAsync(any(), any()) } returns o
+
+        // SUT
+        assertNotNull(spyCrawler.imagesOnPageLinksAsync(mockPage, 3).toStream().forEach() {})
+
+        verify(exactly = 1) { mockPage.getPageElementsAsStrings(any()) }
+        verify(exactly = 3) { spyCrawler.crawlPageAsync(any(), 4) }
+        verify(exactly = 3) { o.subscribeOn(Schedulers.parallel()) }
+    }
+
+    @Test
+    fun `imagesOnPageAsync uses the expected Project Reactor outer chained method calls`() {
+        mockkStatic(Flux::class)
+        every { mockImageFlux.flatMap<Image>(any()) } returns mockImageFlux
+        every { mockUrlFlux.flatMap<Image>(any()) } returns mockImageFlux
+        every { Flux.fromIterable<URL>(any()) } returns mockUrlFlux
+
+        // SUT
+        assertThat(spyCrawler.imagesOnPageAsync(mockPage)).isSameAs(mockImageFlux)
+
+        verify { mockUrlFlux.flatMap<Image>(any()) }
+        verify { mockImageFlux.flatMap<Image>(any()) }
+        verify { Flux.fromIterable<URL>(any()) }
+        verify(exactly = 0) { mockImageFlux.subscribeOn(any()) }
+    }
+
+    @Test
+    fun `imagesOnPageAsync must filter nulls`() {
+        mockkStatic(Optional::class)
+
+        val url1 = URL("http://www.dummy.url-1")
+        val url2 = URL("http://www.dummy.url-2")
+        val url3 = URL("http://www.dummy.url-3")
+        val urls = listOf(url1, url2, url3)
+        every { mockPage.getPageElementsAsUrls(any()) } returns UnsynchronizedArray(urls)
+        every { spyCrawler.getOrDownloadImage(url1) } returns mockImage
+        every { spyCrawler.getOrDownloadImage(url2) } returns null
+        every { spyCrawler.getOrDownloadImage(url3) } returns mockImage
+        every { spyCrawler.transformImageAsync(any()) } answers {
+            Flux.just(it.invocation.args[0] as Image)
+        }
+
+        // SUT
+        assertEquals(2, spyCrawler.imagesOnPageAsync(mockPage).toStream().count())
+
+        verify(exactly = 3) { spyCrawler.getOrDownloadImage(any()) }
+        verify(exactly = 2) { spyCrawler.transformImageAsync(any()) }
+    }
+
+    @Test
+    fun `imagesOnPageAsync must correctly use the Optional class`() {
+        mockkStatic(Optional::class)
+        val spyOptional = spyk<Optional<Image>>()
+        val spyNullOptional = spyk<Optional<Image>>()
+
+        every { Optional.ofNullable<Image>(any()) } answers {
+            if (invocation.args[0] == null) {
+                spyNullOptional
+            } else {
+                spyOptional
+            }
+        }
+        every { spyOptional.isPresent } returns true
+        every { spyOptional.get() } returns mockImage
+        every { spyNullOptional.isPresent } returns false
+
+        val url1 = URL("http://www.dummy.url-1")
+        val url2 = URL("http://www.dummy.url-2")
+        val url3 = URL("http://www.dummy.url-3")
+        val urls = listOf(url1, url2, url3)
+        every { mockPage.getPageElementsAsUrls(any()) } returns UnsynchronizedArray(urls)
+        every { spyCrawler.getOrDownloadImage(url1) } returns mockImage
+        every { spyCrawler.getOrDownloadImage(url2) } returns null
+        every { spyCrawler.getOrDownloadImage(url3) } returns mockImage
+        every { spyCrawler.transformImageAsync(any()) } answers {
+            Flux.just(it.invocation.args[0] as Image)
+        }
+
+        // SUT
+        assertEquals(2, spyCrawler.imagesOnPageAsync(mockPage).toStream().count())
+
+        verify(exactly = 3) { spyCrawler.getOrDownloadImage(any()) }
+        verify(exactly = 2) { spyCrawler.transformImageAsync(any()) }
+        verify(exactly = 6) { Optional.ofNullable<Image>(any()) }
+        verify(exactly = 2) { spyOptional.isPresent }
+        verify(exactly = 4) { spyNullOptional.isPresent }
+        verify(exactly = 2) { spyOptional.get() }
+    }
+
+    @Test
+    fun `downloadImageAsync uses the expected Project Reactor chained method calls`() {
+        mockkStatic(Mono::class)
+
+        val url = URL("http://www.dummy.url-1")
+        every { spyCrawler.getOrDownloadImage(any()) } throws IllegalStateException(
+            "getOrDownloadImage() is called from within the wrong Project Reactor chain method."
+        )
+
+        every { Mono.fromCallable<URL>(any()) } answers { mockUrlMono }
+        every { mockUrlMono.subscribeOn(any()) } answers { mockUrlMono }
+        every { mockUrlMono.map<Optional<Image>>(any()) } answers { mockOptionalImageMono }
+        every { mockOptionalImageMono.filter(any()) } answers { mockOptionalImageMono }
+        every { mockOptionalImageMono.map<Image>(any()) } answers { mockImageMono }
+
+        // SUT
+        assertThat(spyCrawler.downloadImageAsync(url)).isSameAs(mockImageMono)
+
+        verify(exactly = 1) { mockOptionalImageMono.filter(any()) }
+        verify(exactly = 1) { mockUrlMono.map<Optional<Image>>(any()) }
+        verify(exactly = 1) { Mono.fromCallable<URL>(any()) }
+        verify(exactly = 1) { mockUrlMono.subscribeOn(any()) }
+        verify(exactly = 1) { mockOptionalImageMono.map<Image>(any()) }
+    }
+
+    @Test
+    fun `transformImageAsync uses the expected Project Reactor outer chained method calls`() {
+        mockkStatic(Flux::class)
+        every { Flux.fromIterable<Transform>(any()) } returns mockTransformFlux
+        every { mockOptionalImageFlux.map<Image>(any()) } returns mockImageFlux
+        every { mockTransformFlux.filter(any()) } returns mockTransformFlux
+        every { mockTransformFlux.map<Optional<Image>>(any()) } returns mockOptionalImageFlux
+        every { mockTransformFlux.subscribeOn(any()) } returns mockTransformFlux
+        every { mockOptionalImageFlux.filter(any()) } returns mockOptionalImageFlux
+
+        // SUT
+        spyCrawler.mTransforms = mockTransforms
+        spyCrawler.transformImageAsync(mockImage)
+
+        verify { mockTransformFlux.subscribeOn(Schedulers.parallel()) }
+        verify { Flux.fromIterable(spyCrawler.mTransforms) }
+        verify { mockTransformFlux.filter(any()) }
+        verify { mockTransformFlux.map<Optional<Image>>(any()) }
+        verify { mockOptionalImageFlux.map<Image>(any()) }
+        verify { mockOptionalImageFlux.filter(any()) }
+    }
+
+    @Test
+    fun `transformImageAsync uses the expected Project Reactor inner chained method calls`() {
+        mockkStatic(Optional::class)
+        val mockTransform = mockk<Transform>()
+        listOf(mockTransform, mockTransform, mockTransform).apply {
+            injectInto(spyCrawler)
+        }
+        val total = spyCrawler.mTransforms.count()
+        val expected = total - 2
+
+        val mockOptional = mockk<Optional<Image>>()
+        every { mockOptional.get() } returns mockImage
+        every { Optional.ofNullable<Image>(any()) } returns mockOptional
+        every { spyCrawler.createNewCacheItem(any(), any()) } returns true
+        every { mockOptional.isPresent } returnsMany listOf(false, true, false)
+        every { spyCrawler.applyTransform(any(), any()) } returnsMany listOf(null, mockImage, null)
+
+        // SUT
+        assertEquals(expected, spyCrawler.transformImageAsync(mockImage).toStream().count().toInt())
+
+        verify(exactly = expected) { mockOptional.get() }
+        verify(exactly = total) { spyCrawler.createNewCacheItem(any(), any()) }
+        verify(exactly = total) { mockOptional.isPresent }
+        verify(exactly = total) { spyCrawler.applyTransform(any(), any()) }
+        verify(exactly = expected) { Optional.ofNullable<Image>(mockImage) }
+    }
+}
